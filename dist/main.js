ef3e5b7f793efc46c79e807afdd7928c19f5ea39
Tiago Boeing <tiagoboeing@bol.com.br>
2018-11-18 02:01:50 -0200

    Array de req, resp, next

--- dist/main.js
+++ dist/main.js
@@ -5,10 +5,27 @@ const server = restify.createServer({
     name: 'meat-api',
     version: '1.0.0'
 });
-server.get('/hello', (req, resp, next) => {
-    resp.json({ message: 'hello' });
-    return next();
-});
+server.use(restify.plugins.queryParser());
+server.get('/info', [
+    (req, resp, next) => {
+        // if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+        //     let error: any = new Error()
+        //     error.statusCode = 400
+        //     error.message = 'Atualize seu browser'
+        //     return next(error)
+        // }
+        return next();
+    }, (req, resp, next) => {
+        resp.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+        });
+        return next();
+    }
+]);
 server.listen(3000, () => {
     console.log('API rodando em http://localhost:3000');
 });
