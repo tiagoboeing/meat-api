e2c74429fa4b97ac67d2a75e65765312c01ee392
Tiago Boeing <tiagoboeing@bol.com.br>
2018-11-18 03:34:23 -0200

    Busca por ID, métodos estáticos

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -1,12 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const users = [
-    { name: 'Cara 1', email: 'email@email.com' },
-    { name: 'Cara 2', email: 'email@email.com' }
+    { id: '1', name: 'Cara 1', email: 'email@email.com' },
+    { id: '2', name: 'Cara 2', email: 'email@email.com' }
 ];
 class User {
     static findAll() {
         return Promise.resolve(users);
+    }
+    static findById(id) {
+        return new Promise(resolve => {
+            const filtered = users.filter(user => user.id === id);
+            let user = undefined;
+            if (filtered.length > 0) {
+                user = filtered[0];
+            }
+            resolve(user);
+        });
     }
 }
 exports.User = User;
--- users/users.model.ts
+++ users/users.model.ts
@@ -1,10 +1,21 @@
 const users = [
-    { name: 'Cara 1', email: 'email@email.com' },
-    { name: 'Cara 2', email: 'email@email.com' }
+    { id: '1', name: 'Cara 1', email: 'email@email.com' },
+    { id: '2', name: 'Cara 2', email: 'email@email.com' }
 ]
 
 export class User {
     static findAll(): Promise<any[]>{
         return Promise.resolve(users)
+    }
+
+    static findById(id: string): Promise<any>{
+        return new Promise(resolve => {
+            const filtered = users.filter(user => user.id === id)
+            let user = undefined
+            if(filtered.length > 0){
+                user = filtered[0]
+            }
+            resolve(user)
+        })
     }
 }
