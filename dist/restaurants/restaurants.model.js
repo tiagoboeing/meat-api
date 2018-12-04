e42b53cb91ff14b8b13e750a276a78e18233ca21
Tiago Boeing <tiagoboeing@bol.com.br>
2018-12-04 11:17:06 -0200

    Recurso de restaurantes - schema e subdoc

---
+++ dist/restaurants/restaurants.model.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+const restSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    menu: {
+        type: [menuSchema],
+        required: false,
+        select: false,
+        default: []
+    }
+});
+exports.Restaurant = mongoose.model('Restaurant', restSchema);
+// select: false => não trazer o menu do restaurante por default
